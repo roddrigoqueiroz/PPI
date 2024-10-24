600eaa2858dc0f8da2bd811b378322a3a93d6f72
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2024-10-24 20:40:05 -0300

    Feat: Enviando trabalho 12

---
+++ trabalho12/ex3-cep-await/conexaoMysql.php
@@ -0,0 +1,23 @@
+<?php
+
+function mysqlConnect()
+{
+  $db_host = "sql205.infinityfree.com";
+  $db_username = "if0_37068961";
+  $db_password = "YFs5KEzmC8ffh7";
+  $db_name = "if0_37068961_ppi";
+
+  $options = [
+    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ];
+
+  try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password, $options);
+    return $pdo;
+  } 
+  catch (Exception $e) {
+    exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
+  }
+}
+?>
